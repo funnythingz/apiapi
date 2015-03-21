f1036f7dba495d832948de67b6fd9db6bef12833
funnythingz <funnythingz69+github@gmail.com>
2015-03-21 11:41:46 +0900

    Fixed v1

--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -1,32 +1,28 @@
 module Api
-  module V1
+  class EntriesController < ApplicationController
+    def index
+      @entries = Entry.all
+      render json: @entries
+    end
 
-    class EntriesController < ApplicationController
-      def index
-        @entries = Entry.all
-        render json: @entries
-      end
+    def show
+      @entry = Entry.find_by(id: params[:id])
+    end
 
-      def show
-        @entry = Entry.find_by(id: params[:id])
-      end
+    def create
+      @entry = Entry.new(entry_params)
 
-      def create
-        @entry = Entry.new(entry_params)
-
-        if @entry.save
-          render :show, status: :created, location: @entry
-        else
-          render json: @entry.errors, status: :unprocessable_entity
-        end
-      end
-
-      private
-
-      def entry_params
-        params.require(:entries).permit(:title, :content)
+      if @entry.save
+        render :show, status: :created, location: @entry
+      else
+        render json: @entry.errors, status: :unprocessable_entity
       end
     end
 
+    private
+
+    def entry_params
+      params.require(:entries).permit(:title, :content)
+    end
   end
 end
